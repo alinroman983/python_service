4d67b3f212733f831355ec8397ae681d76a49035
alin <seralin@DESKTOP-5MASV49.localdomain>
2025-07-13 09:58:30 +0300

    Modification on main

--- cli.py
+++ cli.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
-"""This script fetches weather information for a specified location using the wttr.in service."""
+"""This script fetches weather information for a specified
+location using the wttr.in service."""
 
-import requests
 import sys
+import requests
 
 
 def main():
-    """Main fanction to fetch and display weather information.""" 
+    """Main fanction to fetch and display  weather information."""
     if len(sys.argv) == 2:
         location = sys.argv[1]
     else:
         print("Usage: python cli.py <location>")
-        sys.exit(1) 
-
+        sys.exit(1)
     url = f"https://wttr.in/{location}"
-
-    response = requests.get(url)
+    response = requests.get(url, timeout=10)
     print(response.text)
 
 
--- main.py
+++ main.py
@@ -1,26 +1,28 @@
 #!usr/bin/env python3
-"""
-This script fetches weather information for a specified location using the wttr.in service."""
-
+"""This script fetches weather information for a specified
+location using the wttr.in service."""
 from fastapi import FastAPI
 import uvicorn
 import httpx
 
 app = FastAPI()
 
+
 @app.get("/")
 async def read_root():
+    """Root endpoint to welcome users."""
     return {"message": "Welcome to the Weather API"}
+
 
 @app.get("/weather/{location}")
 async def get_weather(location: str):
+    """Fetch weather information for a specified location."""
     url = f"https://wttr.in/{location}"
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
         if response.status_code == 200:
-            return {"location": location, "weather": response.text}
-        else:
-            return {"error": "Could not retrieve weather data", "status_code": response.status_code }
-        
+            return {"lookup": location, "weather": response.text}
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000) 
+    uvicorn.run(app, host="0.0.0.0", port=8000)
