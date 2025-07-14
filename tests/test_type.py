3e66d95dd6fb3aa6baa7eda8a2e0320dfe77b7eb
alin <seralin@DESKTOP-5MASV49.localdomain>
2025-07-14 19:41:45 +0300

    Added pre-commit check

--- tests/test_type.py
+++ tests/test_type.py
@@ -1,8 +1,6 @@
-import pytest
 from type_test import add
+
 
 def test_add():
     assert add(5, 10) == 15
-    assert add("Hello, ", "World!") == "Hello, World!" 
-   
-    
+    assert add("Hello, ", "World!") == "Hello, World!"
