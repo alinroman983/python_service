a8e4fe20ab5375c629a663c5fe2e1c16866fa842
alin <seralin@DESKTOP-5MASV49.localdomain>
2025-07-14 19:53:27 +0300

    Added pre-commit check

---
+++ src/type_test.py
@@ -0,0 +1,3 @@
+def add(a, b):
+    """Adds two values together."""
+    return a + b
--- tests/test_type.py
+++ tests/test_type.py
@@ -1,6 +1,9 @@
+"""This file is a simple test for the add function"""
+
 from type_test import add
 
 
 def test_add():
+    """Some random tests"""
     assert add(5, 10) == 15
     assert add("Hello, ", "World!") == "Hello, World!"
